63bfe43815480c2f11ff523865a65a66e017c15e
Taley'a Mirza <taley.mirza@gmail.com>
2020-10-07 12:47:16 +0500

    feat: add routing

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import React from 'react';
+import { Route, Switch, Link, BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
+import AboutUs from './Components/AboutUs/AboutUs';
+import Contact from './Components/Contact/Contact';
 import Home from './Components/Home/Home';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Simple SPA</h1>
-      <ul className="header">
-        <li><a href="/">Home</a></li>
-        <li><a href="/about">About</a></li>
-        <li><a href="/contact">Contact</a></li>
-      </ul>
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        <h1>Simple SPA</h1>
+        <ul className="header">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
+        <div className="content">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={AboutUs} />
+            <Route path="/contact" component={Contact} />
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
